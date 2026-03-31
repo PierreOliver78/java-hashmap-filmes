0f4d8997ea51bde12c13dd588bfe49f5d543feaa
olive <olive@Oliveira>
2026-03-31 09:11:48 -0300

    Criado para Filmes: Atributos | Construtor | Getter & Setter

--- java-hashmap-filmes/src/com/pierre/hashfilmes/model/Filmes.java
+++ java-hashmap-filmes/src/com/pierre/hashfilmes/model/Filmes.java
@@ -1,7 +1,43 @@
 package com.pierre.hashfilmes.model;
 
 public class Filmes {
-	
-	
+
+	private String titulo;
+	private String genero;
+	private double nota;
+
+	public void Filme(String titulo, String genero, double nota) {
+
+		this.titulo = titulo;
+		this.genero = genero;
+		this.nota = nota;
+		
+		
+		
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
 
 }
