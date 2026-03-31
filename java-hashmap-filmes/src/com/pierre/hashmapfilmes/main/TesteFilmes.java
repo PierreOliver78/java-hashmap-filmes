a19cec4714cd1a09a58ca4a00b468294435aad03
olive <olive@Oliveira>
2026-03-31 09:50:53 -0300

    Criado objetos filmes

--- java-hashmap-filmes/src/com/pierre/hashmapfilmes/main/TesteFilmes.java
+++ java-hashmap-filmes/src/com/pierre/hashmapfilmes/main/TesteFilmes.java
@@ -6,7 +6,16 @@ public class TesteFilmes {
 	
 	public static void main(String[] args) {
 		
-		Filme titulo01 = new Filme("Matrix", "Ficção", 9.0);
+		Filme fime01 = new Filme("Matrix", "Ficção", 9.8);
+		Filme filme02 = new Filme("Titanic", "Drama", 9.5);
+		Filme filme03 = new Filme("Mad Max", "Ficção", 8.9);
+		Filme filme04 = new Filme("Tubarão", "Suspense", 8.3);
+		Filme filme05 = new Filme("Vingadores", "Ficção", 8.9);
+		Filme filme06 = new Filme("Tempo de Matar", "Suspense", 7.8);
+		Filme filme07 = new Filme("Os Goonies", "Aventura", 7.7);
+		Filme filme08 = new Filme("A Teoria de Tudo", "Drama", 7.7);
+		
+		
 		
 			
 	}
